94efabe68c1b0861f29fb00d27ef714abffef784
bolima21 <95056223+bolima21@users.noreply.github.com>
2024-12-13 15:00:51 -0300

    Add files via upload

--- trabalho.sh
+++ trabalho.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+#Q1
+sudo groupadd aluno
+sudo groupadd professor
+sudo groupadd gerente
+#Q2
+sudo mkdir -p /srv
+sudo chown root:gerente /srv
 
-
+#Q3
+sudo mkdir -p /srv/alunos /srv/professores
+sudo chown root:aluno /srv/alunos
+sudo chown root:professor /srv/professores
